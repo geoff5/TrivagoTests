d4a84193be2092a781294f6922fc3becca629e47
geoff5 <geoffoa@yahoo.co.uk>
2018-03-23 19:54:57 +0000

    Added Driver class

---
+++ TrivagoTests/Driver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+
+
+namespace TrivagoTests
+{
+    public class Driver
+    {
+        string url = String.Empty;
+        string browser = String.Empty;
+        IWebDriver myDriver;
+
+        public void initialise()
+        {
+            url = ConfigurationManager.AppSettings["TrivagoURL"].ToString();
+
+            driverChoice(ConfigurationManager.AppSettings["Browser"].ToString().ToLower());
+            myDriver.Manage().Window.Maximize();
+            myDriver.Navigate().GoToUrl(url);
+        }
+
+        private void driverChoice(string browser)
+        {
+            switch(browser)
+            {
+                case "chrome":
+                    myDriver = new ChromeDriver();
+                    break;
+
+                case "phantomjs":
+                    myDriver = new PhantomJSDriver();
+                    break;
+                default:
+                    throw new Exception("An invalid browser was specified in the config file");
+            }
+        }
+    }
+}
