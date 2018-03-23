51cedf61a4b5fe16ad84a0e1ae907d6b6ad9d6ab
geoff5 <geoffoa@yahoo.co.uk>
2018-03-23 22:18:53 +0000

    Added a date utility class

---
+++ TrivagoTests/Utils/DateUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrivagoTests.Utils
+{
+    public static class DateUtility
+    {
+        public static DateTime calculateThreeMonthsAhead()
+        {
+            DateTime today = new DateTime();
+            DateTime threeMonthsAhead = new DateTime();
+
+            today = DateTime.Today;
+            threeMonthsAhead = today.AddMonths(3);
+            return threeMonthsAhead;
+        }
+
+        public static string getMonthText(int month)
+        {
+            string[] months = { "", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            return months[month];
+        }
+    }
+}
