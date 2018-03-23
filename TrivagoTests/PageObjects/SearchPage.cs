e8e840a23eee0acc2bee4df22420165ffd40bcf2
geoff5 <geoffoa@yahoo.co.uk>
2018-03-23 21:23:08 +0000

    Added search page object and filter test class

---
+++ TrivagoTests/PageObjects/SearchPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace TrivagoTests.PageObjects
+{
+    class SearchPage
+    {
+        private IWebDriver driver;
+        
+
+        [FindsBy(How = How.Id, Using = "horus-querytext")]
+        private IWebElement searchBar;
+
+        [FindsBy(How = How.ClassName, Using = "horus-btn-search__label")]
+        private IWebElement searchBtn;
+
+        public SearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void enterSearchLocation(string location)
+        {
+            searchBar.Clear();
+            searchBar.SendKeys(location);
+        }
+
+        public void clickSearchBtn()
+        {
+            searchBtn.Click();
+        }
+    }
+}
---
+++ TrivagoTests/Tests/FilterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using TrivagoTests.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrivagoTests;
+
+namespace TrivagoTests.Tests
+{
+    [TestClass]
+    public class FilterTests
+    {
+        WebDriverWait wait;
+        SearchPage searchPage;
+
+        [TestInitialize]
+        public void initialise()
+        {
+            Driver.initialise();
+            searchPage = new SearchPage(Driver.myDriver);
+            wait = new WebDriverWait(Driver.myDriver, TimeSpan.FromSeconds(60));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("horus-querytext")));
+        }
+
+        [TestMethod]
+        public void wifiFilterTest()
+        {
+            searchPage.enterSearchLocation("Cork");
+            searchPage.clickSearchBtn();
+            Thread.Sleep(10000);
+        }
+
+        [TestCleanup]
+        public void cleanUpTest()
+        {
+            Driver.myDriver.Quit();
+        }
+    }
+}
